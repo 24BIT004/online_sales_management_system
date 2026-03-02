ef172525a2f19ec4262f9d5790f1132d60fa8c6c
24BIT004 <zololwairene@gmail.com>
2026-03-02 16:54:00 +0300

    gg

--- Backend/OnlineSalesManagementSystem/urls.py
+++ Backend/OnlineSalesManagementSystem/urls.py
@@ -5,5 +5,6 @@ from App1.views import home
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
+    path("", include("App1.urls")),
     path("api/", include("App1.urls")),
 ]
